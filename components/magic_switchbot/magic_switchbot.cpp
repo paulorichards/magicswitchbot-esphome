123101a16cb80001f97fd3b71872a55b0aaa1ef7
Paulo Richards <paulo.richards@gmail.com>
2021-08-04 23:38:46 -0400

    Client implementation

--- components/magic_switchbot/magic_switchbot.cpp
+++ components/magic_switchbot/magic_switchbot.cpp
@@ -14,6 +14,7 @@ void MagicSwitchBot::dump_config() {
 }
 
 void MagicSwitchBot::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) {
+    ESP_LOGW(TAG, "[%s] EVENT!!!", this->get_name().c_str());
   switch (event) {
     case ESP_GATTC_DISCONNECT_EVT: {
       this->battery_level_ = NAN;
