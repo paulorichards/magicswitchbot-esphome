23a52508f5a9a3d436c0c6023eb02b7793227800
Paulo Richards <paulo.richards@gmail.com>
2021-08-06 19:18:35 -0400

    Client implementation

--- components/magic_switchbot/__init__.py
+++ components/magic_switchbot/__init__.py
@@ -1,14 +1,9 @@
   
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, ble_client
+from esphome.components import ble_client
 from esphome.const import (
-    CONF_ID, 
-    CONF_BATTERY_LEVEL, 
-    UNIT_PERCENT, 
-    ICON_EMPTY, 
-    DEVICE_CLASS_BATTERY, 
-    STATE_CLASS_MEASUREMENT
+    CONF_ID
 )
 
 
@@ -16,20 +11,13 @@ DEPENDENCIES = ["ble_client"]
 
 magic_switchbot_ns = cg.esphome_ns.namespace("magic_switchbot")
 MagicSwitchBot = magic_switchbot_ns.class_(
-    "MagicSwitchBot", sensor.Sensor, ble_client.BLEClientNode, cg.Component
+    "MagicSwitchBot", ble_client.BLEClientNode, cg.Component
 )
 
 CONFIG_SCHEMA = ( 
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(MagicSwitchBot),
-            cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(
-                UNIT_PERCENT,
-                ICON_EMPTY,
-                0,
-                DEVICE_CLASS_BATTERY,
-                STATE_CLASS_MEASUREMENT,
-            ),
         }
     )
     .extend(ble_client.BLE_CLIENT_SCHEMA)
@@ -40,6 +28,3 @@ async def to_code(config):
     await cg.register_component(var, config)
     await ble_client.register_ble_node(var, config)
  
-    if CONF_BATTERY_LEVEL in config:
-        sens = await sensor.new_sensor(config[CONF_BATTERY_LEVEL])
-        cg.add(var.set_battery_level(sens))
