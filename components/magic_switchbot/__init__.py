706c8969142a4a7518d22035c4e298ba639f3176
Paulo Richards <paulo.richards@gmail.com>
2021-08-06 19:04:18 -0400

    Client implementation

---
+++ components/magic_switchbot/__init__.py
@@ -0,0 +1,46 @@
+  
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, ble_client
+from esphome.const import (
+    CONF_ID, 
+    CONF_BATTERY_LEVEL, 
+    UNIT_PERCENT, 
+    ICON_EMPTY, 
+    DEVICE_CLASS_BATTERY, 
+    STATE_CLASS_MEASUREMENT
+)
+
+
+DEPENDENCIES = ["ble_client"]
+
+magic_switchbot_ns = cg.esphome_ns.namespace("magic_switchbot")
+MagicSwitchBot = magic_switchbot_ns.class_(
+    "MagicSwitchBot", sensor.Sensor, ble_client.BLEClientNode, cg.Component
+)
+
+CONFIG_SCHEMA = ( 
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(MagicSwitchBot),
+            cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(
+                UNIT_PERCENT,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_BATTERY,
+                STATE_CLASS_MEASUREMENT,
+            ),
+        }
+    )
+    .extend(ble_client.BLE_CLIENT_SCHEMA)
+    .extend(cv.COMPONENT_SCHEMA)
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await ble_client.register_ble_node(var, config)
+ 
+    if CONF_BATTERY_LEVEL in config:
+        sens = await sensor.new_sensor(config[CONF_BATTERY_LEVEL])
+        cg.add(var.set_battery_level(sens))
--- components/magic_switchbot/magic_switchbot.h
+++ components/magic_switchbot/magic_switchbot.h
@@ -10,14 +10,13 @@
 namespace esphome {
 namespace magic_switchbot {
 
-class MagicSwitchBot : public Component, public sensor::Sensor, public esphome::ble_client::BLEClientNode, public CustomAPIDevice {
+class MagicSwitchBot : public Component, public esphome::ble_client::BLEClientNode, public CustomAPIDevice {
  public:
   void setup() override;
   void loop() override;
   void update() override;
   void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
                            esp_ble_gattc_cb_param_t *param) override;
-  void set_address(uint64_t address) { address_ = address; };
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
   void set_battery_level(sensor::Sensor *battery_level) { battery_level_ = battery_level; }
