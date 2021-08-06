f3951ec212acc2cd1024f7dd8d024c2bfca2b333
Paulo Richards <paulo.richards@gmail.com>
2021-08-06 19:23:12 -0400

    Client implementation

--- components/magic_switchbot/magic_switchbot.cpp
+++ components/magic_switchbot/magic_switchbot.cpp
@@ -10,7 +10,6 @@ static const char *const TAG = "magic_switchbot";
 
 void MagicSwitchBot::dump_config() {
   ESP_LOGCONFIG(TAG, "Magic SwitchBot");
-  LOG_SENSOR("  ", "Battery Level", this->battery_level_);
 }
 
 void MagicSwitchBot::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) {
--- components/magic_switchbot/magic_switchbot.h
+++ components/magic_switchbot/magic_switchbot.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/esp32_ble_tracker/esp32_ble_tracker.h"
 
 #ifdef ARDUINO_ARCH_ESP32
@@ -10,7 +9,7 @@
 namespace esphome {
 namespace magic_switchbot {
 
-class MagicSwitchBot : public Component, public esphome::ble_client::BLEClientNode, public CustomAPIDevice {
+class MagicSwitchBot : public esphome::ble_client::BLEClientNode, public CustomAPIDevice {
  public:
   void setup() override;
   void loop() override;
@@ -19,11 +18,8 @@ class MagicSwitchBot : public Component, public esphome::ble_client::BLEClientNo
                            esp_ble_gattc_cb_param_t *param) override;
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
-  void set_battery_level(sensor::Sensor *battery_level) { battery_level_ = battery_level; }
-
+  
  protected:
-  uint64_t address_;
-  sensor::Sensor *battery_level_{nullptr};
 };
 
 }  // namespace magic_switchbot
