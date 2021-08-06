415cef6c7c28d7050bd363faae3a1b1ccb00f02a
Paulo Richards <paulo.richards@gmail.com>
2021-08-06 19:57:58 -0400

    Client implementation

--- components/magic_switchbot/magic_switchbot.h
+++ components/magic_switchbot/magic_switchbot.h
@@ -12,9 +12,6 @@ namespace magic_switchbot {
 
 class MagicSwitchbot : public esphome::ble_client::BLEClientNode {
  public:
-  void setup() override;
-  void loop() override;
-  void update() override;
   void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
                            esp_ble_gattc_cb_param_t *param) override;
   void dump_config() override;
