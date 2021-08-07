091a26fe0ecc0ca125efc5886975af3f0163e850
Paulo Richards <paulo.richards@gmail.com>
2021-08-06 20:11:11 -0400

    Client implementation

--- components/magic_switchbot/magic_switchbot.cpp
+++ components/magic_switchbot/magic_switchbot.cpp
@@ -16,7 +16,7 @@ void MagicSwitchbot::loop(){
 
 }
 
-void MagicSwitchBot::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) {
+void MagicSwitchbot::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) {
   switch (event) {
     ESP_LOGW(TAG, "Event recived");
   }
